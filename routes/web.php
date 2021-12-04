d2c33b1b4360d5298b3f68536b887e2cbdb99b41
konyahin.d <konyahin.d@stis.ru>
2021-12-04 18:10:47 +0300

    done

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['before' => '/{any}', 'where' => ['any' => '.*']], function () {
+    Route::get('/{any}', function () {
+        return view('welcome');
+    })->where('any', '.*');
 });
-// Route::group(['before' => '/{any}', 'where' => ['any' => '.*']], function () {
-//     Route::get('/{any}', function () {
-//         return view('welcome');
-//     })->where('any', '.*');
-// });
 
 
